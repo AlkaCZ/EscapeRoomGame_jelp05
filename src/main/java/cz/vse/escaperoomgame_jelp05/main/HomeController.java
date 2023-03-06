94d2562d4a88a74ce566398a1a7665888eea2fbe
Petr Jelinek <petrjelinek35@seznam.cz>
2023-03-06 22:15:57 +0100

    Dopracování základních funkcí

--- src/main/java/cz/vse/escaperoomgame_jelp05/main/HomeController.java
+++ src/main/java/cz/vse/escaperoomgame_jelp05/main/HomeController.java
@@ -5,9 +5,9 @@ import cz.vse.escaperoomgame_jelp05.logika.IHra;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.Optional;
 
 public class HomeController {
     @FXML
@@ -36,6 +36,14 @@ public class HomeController {
         vystup.appendText(hra.vratEpilog());
         vstup.setDisable(true);
         odesliButton.setDisable(true);
+        }
+    }
+
+    public void ukoncitHru(ActionEvent actionEvent) {
+        Alert ukonceni = new Alert(Alert.AlertType.CONFIRMATION, "Opravdu chceš ukončit hru ?");
+        Optional<ButtonType> result = ukonceni.showAndWait();
+        if (result.isPresent() && result.get()  == ButtonType.OK){
+            Platform.exit();
         }
     }
 }
